5a2da03dca0291867b2e6af3f5d1b79ccbc03d65
rvm2113 <manohr@rpi.edu>
2018-12-03 15:09:19 -0500

    user_account fiasco

--- useraccount.js
+++ useraccount.js
@@ -1,7 +1,40 @@
 
+
+
+
 function search_for_books(){
 	document.location.href = "index.php";
 }
+
+function transfer_ownership(id){
+	var total_id = 'myModal' + id;
+	var modal = document.getElementById(total_id);
+	console.log("MODAL: " + modal);
+	modal.style.display = "block";
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close");
+
+	for(var i = 0; i < span.length; ++i){
+		span[i].onclick = function() {
+	    	modal.style.display = "none";
+		}
+	}
+	// When the user clicks on <span> (x), close the modal
+	// span.
+
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+	    if (event.target == modal) {
+	        modal.style.display = "none";
+	    }
+	}
+
+  
+}
+
+
 
 window.onload = function(){
 
@@ -9,6 +42,15 @@ window.onload = function(){
 
 
 }
+
+
+
+
+
+
+
+
+
 
 $("#booksbtn").onclick = function(){
 	document.location.href = "http://localhost/BookShare/index.php";
